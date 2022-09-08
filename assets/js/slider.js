59441dae3bddf4fabdb8e0d0c7776cc0fafc2caa
franncho <rarugfrancisco@gmail.com>
2022-09-08 18:41:50 -0300

    WIP slider tradicional

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,192 +1,76 @@
-let carousel = document.querySelector('.carousel');
-
-let carouselInner = document.querySelector('.carousel-inner');
-
-let prev = document.querySelector('.carousel-controls .prev');
-
-let next = document.querySelector('.carousel-controls .next');
-
-let slides =  document.querySelectorAll('.carousel-inner .carousel-item');
-
-let totalSlides = slides.length;
-
-let step = 100 / totalSlides;
-
-let activeSlide = 0;
-
-let activeIndicator = 0;
-
-let direction = -1;
-
-let jump = 1;
-
-let interval = 5000;
-
-let time;
-
-
-
-//Init carousel
-carouselInner.style.minWidth = (totalSlides * 100) + '%';
-loadIndicators();
-loop(true);
-
-
-//Carousel events
-
-next.addEventListener('click',()=>{
-    slideToNext();
-});
-
-prev.addEventListener('click',()=>{
-    slideToPrev();
-});
-
-carouselInner.addEventListener('transitionend',()=>{
-    if(direction === -1){
-        if(jump > 1){
-            for(let i = 0; i < jump; i++){
-                activeSlide++;
-                carouselInner.append(carouselInner.firstElementChild);
-            }
-        }else{
-            activeSlide++;
-            carouselInner.append(carouselInner.firstElementChild);
-        }
-    }else if(direction === 1){
-        if(jump > 1){
-            for(let i = 0; i < jump; i++){
-                activeSlide--;
-                carouselInner.prepend(carouselInner.lastElementChild);
-            }
-        }else{
-            activeSlide--;
-            carouselInner.prepend(carouselInner.lastElementChild);
-        }
-    };
-
-    carouselInner.style.transition = 'none';
-    carouselInner.style.transform = 'translateX(0%)';
-    setTimeout(()=>{
-        jump = 1;
-        carouselInner.style.transition = 'all ease .5s';
+const carouselSlider = (function () {
+    let _slideIndex = 1;
+  
+    const _slides = document.querySelectorAll(".image-fade");
+    const _dots = document.querySelectorAll(".dot");
+  
+    function _sliderInitState(slides, dots) {
+      for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+  
+      for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.remove("active");
+      }
+    }
+  
+    function _checkSlideIndexBoundary(slideIndex, slides) {
+      if (slideIndex > slides.length) {
+        _slideIndex = 1;
+      }
+      if (slideIndex === 0) {
+        _slideIndex = slides.length;
+      }
+    }
+  
+    function _slideSelectionIndecator(dots, slideIndex) {
+      dots[slideIndex - 1].classList.add("active");
+    }
+  
+    function _activateSlide(slides, slideIndex) {
+      slides[slideIndex - 1].style.display = "block";
+      _slideSelectionIndecator(_dots, _slideIndex);
+    }
+  
+    function _imageDirection(direction) {
+      _sliderInitState(_slides, _dots);
+      if (direction == "next") {
+        _activateSlide(_slides, _slideIndex);
+        _slideIndex++;
+        _checkSlideIndexBoundary(_slideIndex, _slides);
+      } else {
+        _slideIndex--;
+        _checkSlideIndexBoundary(_slideIndex, _slides);
+        _activateSlide(_slides, _slideIndex);
+      }
+    }
+  
+    const _previousButton = document.querySelector(".previous");
+    const _nextButton = document.querySelector(".next");
+  
+    _previousButton.addEventListener("click", function () {
+      _imageDirection("previous");
     });
-    updateIndicators();
-});
-
-document.querySelectorAll('.carousel-indicators span').forEach(item=>{
-    item.addEventListener('click',(e)=>{
-        let slideTo = parseInt(e.target.dataset.slideTo);
-        
-        let indicators = document.querySelectorAll('.carousel-indicators span');
-
-        indicators.forEach((item,index)=>{
-            if(item.classList.contains('active')){
-                activeIndicator = index
-            }
-        })
-
-        if(slideTo - activeIndicator > 1){
-            jump = slideTo - activeIndicator;
-            step = jump * step;
-            slideToNext();
-        }else if(slideTo - activeIndicator === 1){
-            slideToNext();
-        }else if(slideTo - activeIndicator < 0){
-
-            if(Math.abs(slideTo - activeIndicator) > 1){
-                jump = Math.abs(slideTo - activeIndicator);
-                step = jump * step;
-                slideToPrev();
-            }
-                slideToPrev();
-        }
-        step = 100 / totalSlides; 
-    })
-});
-
-carousel.addEventListener('mouseover',()=>{
-    loop(false);
-})
-
-carousel.addEventListener('mouseout',()=>{
-    loop(true);
-})
-
-//Carousel functions
-
-function loadIndicators(){
-    slides.forEach((slide,index)=>{
-        if(index === 0){
-            document.querySelector('.carousel-indicators').innerHTML +=
-            `<span data-slide-to="${index}" class="active"></span>`;
-        }else{
-            document.querySelector('.carousel-indicators').innerHTML +=
-            `<span data-slide-to="${index}"></span>`;
-        }
-    }); 
-};
-
-function updateIndicators(){
-    if(activeSlide > (totalSlides - 1)){
-        activeSlide = 0;
-    }else if(activeSlide < 0){
-        activeSlide = (totalSlides - 1);
-    }
-    document.querySelector('.carousel-indicators span.active').classList.remove('active');
-    document.querySelectorAll('.carousel-indicators span')[activeSlide].classList.add('active');
-};
-
-function slideToNext(){
-    if(direction === 1){
-        direction = -1;
-        carouselInner.prepend(carouselInner.lastElementChild);
+  
+    _nextButton.addEventListener("click", function () {
+      _imageDirection("next");
+    });
+  
+    const slide = function () {
+      _sliderInitState(_slides, _dots);
+      _activateSlide(_slides, _slideIndex);
+      _slideIndex++;
+      _checkSlideIndexBoundary(_slideIndex, _slides);
+      // Change image every 5 seconds
+      setTimeout(slide, 5000);
     };
-    
-    carousel.style.justifyContent = 'flex-start';
-    carouselInner.style.transform = `translateX(-${step}%)`;
-};
-
-function slideToPrev(){
-    if(direction === -1){
-        direction = 1;
-        carouselInner.append(carouselInner.firstElementChild);
+  
+    return {
+      slide
     };
-    carousel.style.justifyContent = 'flex-end'
-    carouselInner.style.transform = `translateX(${step}%)`;
-};
-
-function loop(status){
-    if(status === true){
-        time = setInterval(()=>{
-            slideToNext();
-        },interval);
-    }else{
-        clearInterval(time);
-    }
-}
-
-
-try {
-  fetch(new Request("https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", { method: 'HEAD', mode: 'no-cors' })).then(function(response) {
-    return true;
-  }).catch(function(e) {
-    var carbonScript = document.createElement("script");
-    carbonScript.src = "//cdn.carbonads.com/carbon.js?serve=CE7DC2JW&placement=wwwcssscriptcom";
-    carbonScript.id = "_carbonads_js";
-    document.getElementById("carbon-block").appendChild(carbonScript);
-  });
-} catch (error) {
-  console.log(error);
-}
-
-
-
-
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-46156385-1', 'cssscript.com');
-  ga('send', 'pageview');
+  })();
+  
+  carouselSlider.slide();
+  
+  
+  
