0c53cc5b18808ca71cc740c59849cf522c3b93be
franncho <rarugfrancisco@gmail.com>
2022-09-08 18:02:58 -0300

    slider 3d

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,123 +1,192 @@
-// // Modified from: How to code a carousel by Kevin Powell https://www.youtube.com/watch?v=gBzsE0oieio
-const track = document.querySelector('.carousel__track');  // select the ul
-const slides = Array.from(track.children);
-const slidesLength = slides.length; // get length of array (number of images)
-var counter = 0; // check postion in slides 
-const nextButton = document.querySelector('.carousel__button--right');
-const prevButton = document.querySelector('.carousel__button--left');
-const dotsNav = document.querySelector('.carousel__nav');
-const dots = Array.from(dotsNav.children);
-const slideWidth= slides[0].getBoundingClientRect().width; // get size of the first slide (same for all slides),  get only width of the first slide
+let carousel = document.querySelector('.carousel');
 
-const totalWidth = slideWidth * (slidesLength -1) + 'px'; // get total width of all images (when placed next to each other)
-// arrange the slides next to one another
-// make a function
-const setSlidePosition = (slide, index) => {
-    slide.style.left = slideWidth * index + 'px';  // change style --> move slide from the left (shift from absolute pos.)
-}
+let carouselInner = document.querySelector('.carousel-inner');
 
-slides.forEach(setSlidePosition); // call function for each slide
+let prev = document.querySelector('.carousel-controls .prev');
 
-const moveToSlide = (track, currentSlide, targetSlide) => {
-    track.style.transform = 'translateX(-' + targetSlide.style.left + ')'; // shift ul
-    currentSlide.classList.remove('current-slide'); // remove .current-slide CSS on current slide
-    targetSlide.classList.add('current-slide'); // add .current-slide CSS on next slide 
-}
+let next = document.querySelector('.carousel-controls .next');
 
-const updateDots = (currentDot, targetDot) => {
-    currentDot.classList.remove('current-slide');
-    targetDot.classList.add('current-slide');
-}
+let slides =  document.querySelectorAll('.carousel-inner .carousel-item');
 
-// When prev clicked, move to slide on the left
-prevButton.addEventListener('click', e => {
-    counter --; // decr counter (for checking slide pos)
-    const currentSlide = track.querySelector('.current-slide');
-    const prevSlide = currentSlide.previousElementSibling;
-    const currentDot = dotsNav.querySelector('.current-slide');
-    const prevDot = currentDot.previousElementSibling;
+let totalSlides = slides.length;
 
-    // if prev clicked when on the first slide 
-    if ((counter) == -1) {
-        const lastSlide = slides[slidesLength - 1];
-        const lastDot = dots[slidesLength - 1];
-        counter = slidesLength - 1;  // set counter to last slide index
-        track.style.transform = 'translateX(-' + totalWidth + ')'; // shift ul to the last slide
-        currentSlide.classList.remove('current-slide'); // remove .current-slide CSS on current slide
-        lastSlide.classList.add('current-slide'); // add .current-slide CSS on the last slide
-        currentDot.classList.remove('current-slide');
-        lastDot.classList.add('current-slide');
-        
-        slideResize(); // call resize function (shifts track correctly)
+let step = 100 / totalSlides;
 
-    } else {
-        // not the first slide
-        moveToSlide(track, currentSlide, prevSlide);
-        updateDots(currentDot, prevDot);
-    }
+let activeSlide = 0;
+
+let activeIndicator = 0;
+
+let direction = -1;
+
+let jump = 1;
+
+let interval = 5000;
+
+let time;
+
+
+
+//Init carousel
+carouselInner.style.minWidth = (totalSlides * 100) + '%';
+loadIndicators();
+loop(true);
+
+
+//Carousel events
+
+next.addEventListener('click',()=>{
+    slideToNext();
 });
 
+prev.addEventListener('click',()=>{
+    slideToPrev();
+});
 
-// When next clicked, move to slide on the right
-nextButton.addEventListener('click', e => {  // track the event (click)
-    counter ++; // incr counter for tracking index pos
-    const currentSlide = track.querySelector('.current-slide');  
-    const nextSlide = currentSlide.nextElementSibling;
-    const currentDot = dotsNav.querySelector('.current-slide')
-    const nextDot = currentDot.nextElementSibling;
+carouselInner.addEventListener('transitionend',()=>{
+    if(direction === -1){
+        if(jump > 1){
+            for(let i = 0; i < jump; i++){
+                activeSlide++;
+                carouselInner.append(carouselInner.firstElementChild);
+            }
+        }else{
+            activeSlide++;
+            carouselInner.append(carouselInner.firstElementChild);
+        }
+    }else if(direction === 1){
+        if(jump > 1){
+            for(let i = 0; i < jump; i++){
+                activeSlide--;
+                carouselInner.prepend(carouselInner.lastElementChild);
+            }
+        }else{
+            activeSlide--;
+            carouselInner.prepend(carouselInner.lastElementChild);
+        }
+    };
 
-    // if the next button is clicked when the current-slide is the last slide...
-    if ((counter) == slidesLength) {
-        const firstSlide = slides[0];
-        const firstDot = dots[0];
-        counter = 0;
-        track.style.transform = 'translateX(-' + 0 + 'px' + ')'; // shift ul to first slide
-        currentSlide.classList.remove('current-slide'); // remove .current-slide CSS on current slide
-        firstSlide.classList.add('current-slide'); // add .current-slide CSS on the first slide
+    carouselInner.style.transition = 'none';
+    carouselInner.style.transform = 'translateX(0%)';
+    setTimeout(()=>{
+        jump = 1;
+        carouselInner.style.transition = 'all ease .5s';
+    });
+    updateIndicators();
+});
 
-        currentDot.classList.remove('current-slide');
-        firstDot.classList.add('current-slide');
+document.querySelectorAll('.carousel-indicators span').forEach(item=>{
+    item.addEventListener('click',(e)=>{
+        let slideTo = parseInt(e.target.dataset.slideTo);
+        
+        let indicators = document.querySelectorAll('.carousel-indicators span');
 
-    } else {
-        // not the last slide...
-        moveToSlide(track, currentSlide, nextSlide);
-        updateDots(currentDot, nextDot);
-    }
+        indicators.forEach((item,index)=>{
+            if(item.classList.contains('active')){
+                activeIndicator = index
+            }
+        })
+
+        if(slideTo - activeIndicator > 1){
+            jump = slideTo - activeIndicator;
+            step = jump * step;
+            slideToNext();
+        }else if(slideTo - activeIndicator === 1){
+            slideToNext();
+        }else if(slideTo - activeIndicator < 0){
+
+            if(Math.abs(slideTo - activeIndicator) > 1){
+                jump = Math.abs(slideTo - activeIndicator);
+                step = jump * step;
+                slideToPrev();
+            }
+                slideToPrev();
+        }
+        step = 100 / totalSlides; 
+    })
+});
+
+carousel.addEventListener('mouseover',()=>{
+    loop(false);
 })
 
-// When I click the nav indicators, move to that slide
-dotsNav.addEventListener('click', e => {
-    // which indicator was clicked on?
-    const targetDot = e.target.closest('button'); // prevents event when carousel__nav clicked (not near carousel__nav buttons)
-    if (!targetDot) return; // if !targetDot... exit function
-
-    const currentSlide = track.querySelector('.current-slide');
-    const currentDot  = dotsNav.querySelector('.current-slide');
-    const targetIndex = dots.findIndex(dot => dot === targetDot); // get index of dot clicked on
-    counter = targetIndex;
-    const targetSlide = slides[targetIndex];
-
-    moveToSlide(track, currentSlide, targetSlide);
-    updateDots(currentDot, targetDot);
+carousel.addEventListener('mouseout',()=>{
+    loop(true);
 })
 
+//Carousel functions
 
-// when window is resized --> get new carousel slide length and total width then reset track pos so that only the current slide is seen
-window.addEventListener('resize', slideResize);
+function loadIndicators(){
+    slides.forEach((slide,index)=>{
+        if(index === 0){
+            document.querySelector('.carousel-indicators').innerHTML +=
+            `<span data-slide-to="${index}" class="active"></span>`;
+        }else{
+            document.querySelector('.carousel-indicators').innerHTML +=
+            `<span data-slide-to="${index}"></span>`;
+        }
+    }); 
+};
 
-function slideResize(){
-    var newSlideWidth = slides[0].getBoundingClientRect().width; // get size of the first slide (same for all slides),  get only width of the first slide
-    var totalWidth = slideWidth * (slidesLength - 1) + 'px'; // get total length of all slides next to each other
-    // arrange the slides next to one another
-    // make a function
-    const setSlidePosition = (slide, index) => { // reset slide positions for new window size
-        slide.style.left = newSlideWidth * index + 'px';  // change style --> move slide from the left (shift from absolute pos.)
+function updateIndicators(){
+    if(activeSlide > (totalSlides - 1)){
+        activeSlide = 0;
+    }else if(activeSlide < 0){
+        activeSlide = (totalSlides - 1);
     }
+    document.querySelector('.carousel-indicators span.active').classList.remove('active');
+    document.querySelectorAll('.carousel-indicators span')[activeSlide].classList.add('active');
+};
+
+function slideToNext(){
+    if(direction === 1){
+        direction = -1;
+        carouselInner.prepend(carouselInner.lastElementChild);
+    };
     
-    slides.forEach(setSlidePosition); // call function for each slide
-    
-    const currentSlide = track.querySelector('.current-slide');
-    var currentIndex = slides.indexOf(currentSlide);
-    var currentTrackPos = newSlideWidth * currentIndex + 'px';
-    track.style.transform = 'translateX(-' + currentTrackPos + ')'; // shift ul
-}    
+    carousel.style.justifyContent = 'flex-start';
+    carouselInner.style.transform = `translateX(-${step}%)`;
+};
+
+function slideToPrev(){
+    if(direction === -1){
+        direction = 1;
+        carouselInner.append(carouselInner.firstElementChild);
+    };
+    carousel.style.justifyContent = 'flex-end'
+    carouselInner.style.transform = `translateX(${step}%)`;
+};
+
+function loop(status){
+    if(status === true){
+        time = setInterval(()=>{
+            slideToNext();
+        },interval);
+    }else{
+        clearInterval(time);
+    }
+}
+
+
+try {
+  fetch(new Request("https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", { method: 'HEAD', mode: 'no-cors' })).then(function(response) {
+    return true;
+  }).catch(function(e) {
+    var carbonScript = document.createElement("script");
+    carbonScript.src = "//cdn.carbonads.com/carbon.js?serve=CE7DC2JW&placement=wwwcssscriptcom";
+    carbonScript.id = "_carbonads_js";
+    document.getElementById("carbon-block").appendChild(carbonScript);
+  });
+} catch (error) {
+  console.log(error);
+}
+
+
+
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-46156385-1', 'cssscript.com');
+  ga('send', 'pageview');
